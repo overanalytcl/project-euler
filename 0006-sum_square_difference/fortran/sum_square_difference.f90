861e6d062cf9afd150573f460ad3ebc6acf68bd5
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-08 21:36:56 +0300

    fix(0006): increase range to 10^18 ~ 2^64

--- 0006-sum_square_difference/fortran/sum_square_difference.f90
+++ 0006-sum_square_difference/fortran/sum_square_difference.f90
@@ -1,7 +1,8 @@
 program sum_square_difference
    implicit none
 
-   integer, parameter :: n = 100
+   integer, parameter :: kind = selected_int_kind (18)
+   integer(kind=kind), parameter :: n = 100_kind
 
    ! (n(n + 1)/2)^2 - n(n+1)(2n+1)/6
    print *, (n * (n + 1) * (3 * n + 2) * (n - 1) / 12)
