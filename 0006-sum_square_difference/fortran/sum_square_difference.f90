1d0795c8e56dfae3f0de17984859e5739f9c7887
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-09 14:04:15 +0300

    refactor: rename kind to knd to avoid collisions

--- 0006-sum_square_difference/fortran/sum_square_difference.f90
+++ 0006-sum_square_difference/fortran/sum_square_difference.f90
@@ -1,8 +1,8 @@
 program sum_square_difference
    implicit none
 
-   integer, parameter :: kind = selected_int_kind (18)
-   integer(kind=kind), parameter :: n = 100_kind
+   integer, parameter :: knd = selected_int_kind (18)
+   integer(kind=knd), parameter :: n = 100_knd
 
    ! (n(n + 1)/2)^2 - n(n+1)(2n+1)/6
    print *, (n * (n + 1) * (3 * n + 2) * (n - 1) / 12)
