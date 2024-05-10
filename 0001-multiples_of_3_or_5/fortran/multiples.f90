5d9879da493c2f7ce51d2611f1c3f44db2a5344a
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-10 21:29:29 +0300

    feat(0001): update CMakeLists and remove gauss_module

--- 0001-multiples_of_3_or_5/fortran/multiples.f90
+++ 0001-multiples_of_3_or_5/fortran/multiples.f90
@@ -1,11 +1,24 @@
 program multiples
-    use gauss_module
-    implicit none
-    
-    integer, parameter :: limit = 1000
-    integer :: sum
-    
-    sum = gauss_div(limit, 3) + gauss_div(limit, 5) - gauss_div(limit, 15)
-    
-    print *, "The sum of all multiples of 3 or 5 below", limit, "is:", sum
+   implicit none
+
+   integer, parameter :: limit = 1000
+   integer :: sum
+
+   sum = gauss_div(limit, 3) + gauss_div(limit, 5) - gauss_div(limit, 15)
+
+   print *, "The sum of all multiples of 3 or 5 below", limit, "is:", sum
+
+contains
+   integer function gauss(x)
+      implicit none
+      integer, intent(in) :: x
+
+      gauss = x * (x + 1) / 2
+   end function gauss
+
+   integer function gauss_div(n, div) result(g)
+      implicit none
+      integer, intent(in) :: n, div
+      g = div * gauss((n - 1) / div)
+   end function gauss_div
 end program multiples
