7cd8d015cc96c6a9676a4567f0ee66a41131f353
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-14 04:54:20 +0300

    feat(0014): made the solution ~4x faster

--- 0014-longest_collatz_sequence/fortran/app/main.f90
+++ 0014-longest_collatz_sequence/fortran/app/main.f90
@@ -1,39 +1,42 @@
 program main
-  implicit none
-  integer, parameter :: limit = int(1e6)
-  integer(kind=8) :: n, max_steps, max_num, steps
-  integer(kind=8), dimension(1:limit) :: memo
-  integer(kind=8) :: i
+   implicit none
+   integer, parameter :: limit = int(1e6)
+   integer(kind=8) :: n, max_steps, max_num, steps
+   integer(kind=8), dimension(1:limit) :: memo
+   integer(kind=8) :: i
 
-  memo = 0
+   memo = 0
 
-  max_steps = 0
-  max_num = 0
+   max_steps = 0
+   max_num = 0
 
-  do i = 1, limit
+   do i = 1, limit
       n = i
       steps = 0
 
-      if (memo(n) /= 0) then
-          steps = memo(n)
-      else
-          do while (n /= 1)
-              if (mod(n, 2) == 0) then
-                  n = n / 2
-              else
-                  n = 3 * n + 1
-              end if
-              steps = steps + 1
-          end do
-          memo(i) = steps
-      end if
+      do while (n /= 1)
+         if (n <= size(memo) .and. memo(n) /= 0) then
+            steps = steps + memo(n)
+            exit
+         endif
+
+         if (mod(n, 2) == 0) then
+            n = n / 2
+         else
+            n = 3 * n + 1
+         endif
+         steps = steps + 1
+      end do
+
+      if (i <= size(memo)) memo(i) = steps
 
       if (steps > max_steps) then
-          max_steps = steps
-          max_num = i
+         max_steps = steps
+         max_num = i
       end if
-  end do
+   end do
 
-  print *, "Number with the longest sequence:", max_num
-  print *, "Number of steps:", max_steps
+   print *, "Number with the longest sequence:", max_num
+   print *, "Number of steps:", max_steps
 end program main
+
